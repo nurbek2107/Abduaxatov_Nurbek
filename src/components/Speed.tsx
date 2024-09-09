f40fbf5f83b55f1215226e23c0647eb48728ce1e
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-09-09 20:55:24 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/components/Speed.tsx
+++ src/components/Speed.tsx
@@ -20,7 +20,7 @@ const actions = [
 
 export default function BasicSpeedDial() {
     return (
-        <div className="mt-[-240px]">
+        <div className="mt-[-230px]">
         <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
             <SpeedDial
                 ariaLabel="SpeedDial basic example"
