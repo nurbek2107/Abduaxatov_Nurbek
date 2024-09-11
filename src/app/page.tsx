e1a98903c1acf9a8c998665a3d1929723d322919
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-09-11 20:34:23 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,18 +79,21 @@ export default function Page() {
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
       <section id="hero">
-         <div className="relative">
-      <Button onClick={handleClick}>Trigger Fireworks</Button>
-    </div>
+     
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 flex justify-between">
             <div className="flex-col flex flex-1 space-y-1.5">
+              <div className="flex flex-wrap">
               <BlurFadeText
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
                 yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]}`}
               />
+              <div className="relative">
+                <Button className="bg-[#fff0] text-6xl p-0  hover:bg-[#fff0] " onClick={handleClick}>👋</Button>
+                </div>
+              </div>
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
                 delay={BLUR_FADE_DELAY}
