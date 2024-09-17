068177cd9a49d1b040bae64e4bc29160cea416b8
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-09-17 21:12:29 +0500

    main

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,22 +15,22 @@ import { useEffect } from "react";
 
 export default function Navbar() {
   useEffect(() => {
-    window.$crisp = [];
-    window.CRISP_WEBSITE_ID = "70eb2ee0-be96-463b-8c6e-8df7c87b1ccc";
-
+    window.$crisp = []; 
+    window.CRISP_WEBSITE_ID = "70eb2ee0-be96-463b-8c6e-8df7c87b1ccc"; 
     (function () {
       const d = document;
       const s = d.createElement("script");
       s.src = "https://client.crisp.chat/l.js";
       s.async = true;
-      d.getElementsByTagName("head")[0].appendChild(s);
+      d.getElementsByTagName("head")[0].appendChild(s); 
     })();
   }, []);
 
   return (
     <div className="h-full">
       <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
-      <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] ">
+      <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
+        {/* Navbar Items */}
         {DATA.navbar.map((item) => (
           <DockIcon key={item.href}>
             <Tooltip>
@@ -51,11 +51,12 @@ export default function Navbar() {
             </Tooltip>
           </DockIcon>
         ))}
+
         <Separator orientation="vertical" className="h-full" />
+
+        {/* Social Media Icons */}
         {Object.entries(DATA.contact.social)
-          .filter(([name]) => name !== "Telegram")
-          .filter(([name]) => name !== "tel")
-          .filter(([name]) => name !== "email")
+          .filter(([name]) => name !== "Telegram" && name !== "tel" && name !== "email")
           .filter(([_, social]) => social.navbar)
           .map(([name, social]) => (
             <DockIcon key={name}>
@@ -77,7 +78,10 @@ export default function Navbar() {
               </Tooltip>
             </DockIcon>
           ))}
+
         <Separator orientation="vertical" className="h-full py-2" />
+
+        {/* Theme Toggle */}
         <DockIcon>
           <Tooltip>
             <TooltipTrigger asChild>
